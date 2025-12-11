a5f127de2525708b4c964a72e73121469eeb4e6c
Dedes73 <federico.lepore73@gmail.com>
2025-12-11 23:56:10 +0100

    save 3

---
+++ Assets/js/newList.js
@@ -0,0 +1,7 @@
+const listContainer = document.getElementById("listContainer");
+const newNameInput = document.getElementById("listName");
+const createBtn = document.getElementsById("createList");
+
+createBtn.addEventListener("click", () => {
+  const listName = newNameInput.value.trim(); //list name
+});
