2d39b3ed0e96dfd403401b8cb9f54a0de66d50c6
Dedes73 <leporfede@gmail.com>
2025-12-11 16:13:24 +0100

    save 2

---
+++ Assets/js/script.js
@@ -0,0 +1,8 @@
+const listContainer = document.getElementById("listContainer");
+const todoList = document.getElementById("todoList");
+const newElementInput = document.getElementById("newElement");
+const addElementButton = document.getElementsById("addElement")[0];
+
+addElementButton.addEventListener("click", function() => addElementButton(value) {
+
+})
